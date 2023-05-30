9cea67da5e985477ac14f46406bc3b28f8bfcefb
szewczukk <dmitro.szewczuk@gmail.com>
2023-05-30 19:34:42 +0200

    Create user model, create two basic routes

--- packages/users/index.ts
+++ packages/users/index.ts
@@ -1,9 +1,33 @@
 import express from 'express';
+import assert from 'assert';
+import { hash } from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-	res.send('Hello, world!');
+app.use(express.json());
+
+app.get('/api/v1/users', async (req, res) => {
+	const users = await prisma.user.findMany({});
+
+	res.send(users);
+});
+
+app.post('/api/v1/users', async (req, res) => {
+	const { username, password } = req.body;
+	assert(typeof username === 'string');
+	assert(typeof password === 'string');
+
+	const hashedPassword = await hash(password, 10);
+
+	const user = await prisma.user.create({
+		data: { username, password: hashedPassword },
+	});
+
+	res.status(201).send(user);
 });
 
 app.listen(3000, () => console.log('Listening on 3000..'));
+
+prisma.$disconnect();
